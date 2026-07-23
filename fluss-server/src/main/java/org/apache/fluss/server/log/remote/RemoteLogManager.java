33692d02fbf0bf613cf76871e5bde32825826987
Junfan Zhang <zuston@apache.org>
2026-07-23 18:17:41 +0800

    [server] prevent remote log cache size overflow (#3734)
    Co-authored-by: zhangjunfan <zhangjunfan@qiyi.com>

--- fluss-server/src/main/java/org/apache/fluss/server/log/remote/RemoteLogManager.java
+++ fluss-server/src/main/java/org/apache/fluss/server/log/remote/RemoteLogManager.java
@@ -123,7 +123,7 @@ public class RemoteLogManager implements Closeable {
         this.coordinatorGateway = coordinatorGateway;
         this.logManager = logManager;
         this.remoteLogIndexCachesByDir = MapUtils.newConcurrentHashMap();
-        int cacheSize = (int) conf.get(ConfigOptions.REMOTE_LOG_INDEX_FILE_CACHE_SIZE).getBytes();
+        long cacheSize = conf.get(ConfigOptions.REMOTE_LOG_INDEX_FILE_CACHE_SIZE).getBytes();
         for (File dataDir : localDiskManager.dataDirs()) {
             remoteLogIndexCachesByDir.put(
                     dataDir, new RemoteLogIndexCache(cacheSize, remoteLogStorage, dataDir));
